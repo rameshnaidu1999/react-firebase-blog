67cd2bdf82fff6844a40f7aa54c2d4a7b8c32ac8
siva <sivakumar.s@technoduce.com>
2021-07-26 18:43:51 +0530

    warnings cleared

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import Home from "./pages/Home";
-import Signup from "./pages/Signup";
 import Addproduct from "./admin/Addproduct";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SignIn from "./pages/SignIn";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { auth, provider } from "./config/firebase";
 import { useStateValue } from "./StateProvider";
 
 function App() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Blogs from "../components/Blogs";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function Home() {
